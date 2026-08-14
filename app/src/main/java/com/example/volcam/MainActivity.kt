d7d4c32b2299e88be4852f088e6110372b360f8d
frentebruno75-ctrl <frentebruno75@gmail.com>
2026-08-14 06:08:28 -0300

    Upload do projeto VolCam

---
+++ app/src/main/java/com/example/volcam/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.volcam
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tv = TextView(this)
+        tv.text = "VolCam Ativo e Compilado!"
+        tv.textSize = 24f
+        setContentView(tv)
+    }
+}
